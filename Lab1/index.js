b7d3d07b5353f47e30a520f58a4d2076afbfef4e
Dipesh Chaudhary <dipesh43shi@gmail.com>
2026-07-17 15:45:19 +0530

    Lab 1 started

---
+++ Lab1/index.js
@@ -0,0 +1,19 @@
+//write a function to take your name and print it with hello 
+function sayHello(name)
+{
+    console.log('Hello ${name}');
+}
+cost sayHi =(name)=>
+{
+    console.log('Hi ${name}');
+};
+
+
+const findsum = (a,b)=>{
+    return a+b;
+};
+const sum=(a,b)=>a+b;
+
+sayHello{"Dipesh Cahudhary"};
+sayHi("Dipesh");
+console.log("Sum=",findSum)
