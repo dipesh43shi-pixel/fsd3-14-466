2699dea61b5752e827959947c019d7cb3f0aac38
Dipesh Chaudhary <dipesh43shi@gmail.com>
2026-07-23 16:26:07 +0530

    task 3

---
+++ Lab1/EventLoop/task3.js
@@ -0,0 +1,7 @@
+//ORDER RING 
+console.log("1: start");
+setTimeout(()=> console.log("2: setTimeout"),0);
+setImmediate(() => console.log ("3 : setImmediate"));
+process.nextTick(()=> console.log("4: nextTick"));
+console.log("4: end");
+//IN SOME PROCESSOR IMMEIDIATE AND TIMEOUT MAY CHANGE
