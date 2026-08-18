a53d13642dc0b26437502ce5cc77d501769276d6
Dipesh Chaudhary <dipesh43shi@gmail.com>
2026-08-18 10:11:23 +0530

    ADD APP.JS V1

---
+++ Lab3/app.js
@@ -0,0 +1,15 @@
+import http from "http";
+
+const server =http.createServer();
+
+server .on ('request',(req,res)=> {
+    res.write ("<h1>Welcome to Server Side Programming</h1> ");
+    res.write ("<h2> Nodemon is tracking the files</h2>")
+    res.end();
+});
+
+server.listen(5000,()=> {
+console.log("Server is running");
+});
+
+
