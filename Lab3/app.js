d099dc5832a0ef65e5586ccf58b5f8db3c1fbe3a
Dipesh Chaudhary <dipesh43shi@gmail.com>
2026-08-20 14:30:52 +0530

    Basics fo server

--- Lab3/app.js
+++ Lab3/app.js
@@ -1,12 +1,9 @@
 import http from "http";
 
-const server =http.createServer();
-
-server .on ('request',(req,res)=> {
-    res.write ("<h1>Welcome to Server Side Programming</h1> ");
-    res.write ("<h2> Nodemon is tracking the files</h2>")
-    res.end();
+const server = http.createServer ((req,res) => {
+    res.end("<h2> Welcome to Serverside</h2>");
 });
+
 
 server.listen(5000,()=> {
 console.log("Server is running");
