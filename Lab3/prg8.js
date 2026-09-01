347e7a1dbe0e59c7fb608e608f3416476d618e4f
Dipesh Chaudhary <dipesh43shi@gmail.com>
2026-09-01 10:08:42 +0530

    initial

---
+++ Lab3/prg8.js
@@ -0,0 +1,22 @@
+import http from "http";
+import { createReadStream } from "fs"; 
+import { readFile } from "fs/promises";
+
+const server = http.createServer(async (req, res) => {
+    if(req.url==="/stream") {
+
+    
+  const stream = createReadStream("big.txt", {encoding: "utf : 8"});
+  stream.pipe(res); 
+    } else if (req.url==="/normal") {
+        const text =await readFile("big.txt");
+        res.end(text);
+    } else if (req.url==='/product') {
+        //res.writeHead() status 
+        res.setHeader("content-type " ,"text/html")
+        res.statusCode = 200;
+        const data = createReadStream("product.html");
+        data.pipe(res);
+    }
+});
+server.listen(3000,()=> console.log("Server is Runnning at 3000...."));
